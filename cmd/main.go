6feb4ad64e383ff86a6a2ca488f717297b59a253
Sergio Herrero Cruz <sergioherre96@gmail.com>
2023-06-09 23:29:39 +0200

    Hello + name, using url params

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World!")
+	})
+
+	app.Get("/:name", func(c *fiber.Ctx) error {
+		return c.SendString(fmt.Sprintf("Hello %s!", c.Params("name")))
+	})
+
+	app.Listen(":3000")
+}
